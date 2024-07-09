88fa5916dbb99839f340c2fca68245bed378e039
Roger <73677987+rogerhung1129@users.noreply.github.com>
2024-07-09 14:22:33 +0800

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+# Get the container ID(s) for the ancestor image 'docuseal-app'
+container_ids=$(docker container ps -q --filter ancestor=docuseal-app)
+
+# Stop the container(s) if any are found
+if [ -n "$container_ids" ]; then
+  docker container stop $container_ids
+  docker container rm $container_ids
+  
+else
+  echo "No running containers found for ancestor 'docuseal-app'."
+fi
+
 docker build -t docuseal-app:latest .
-docker-compose up
+Home=localhost docker compose up
